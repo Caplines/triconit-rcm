c4ddbeb3d395de1d77fbd2af8b0e59d53423bf54
deepakcardinal <deepak.dogra@cardinalts.com>
2020-10-14 16:46:15 +0530

    Dynamic PDF UI correction

--- rule_engine/ruleengine-client-app/src/app/components/dynamic_ivf/dynamic_ivf.component.ts
+++ rule_engine/ruleengine-client-app/src/app/components/dynamic_ivf/dynamic_ivf.component.ts
@@ -31,8 +31,8 @@ export class DynamicIVFComponent implements OnInit {
     		{name: "Insurance<br/> Name", type: "fill",value:"abc", dt:false,idn:3,idv:23,ch:1,cv:1,rc:1},
     		{name: "Tax ID", type: "fill",value:"113", dt:false,idn:4,idv:24,ch:1,cv:1,rc:1},
     		{name: "Member<br/>SSN:", type: "fill",value:"525864917", dt:true,idn:5,idv:25,ch:1,cv:1,rc:2},
-    		{name: "Policy <br/>Holder:", type: "fill",value:"Alejandro Vera", dt:true,idn:6,idv:25,ch:1,cv:1,rc:1},
-    		{name: "Patient<br/> DOB:", type: "fill",value:"2005-09-12", dt:true,idn:7,idv:25,ch:1,cv:1,rc:1}
+    		{name: "Policy <br/>Holder:", type: "fill",value:"Alejandro Vera", dt:true,idn:6,idv:26,ch:1,cv:1,rc:1},
+    		{name: "Patient<br/> DOB:", type: "fill",value:"2005-09-12", dt:true,idn:7,idv:27,ch:1,cv:1,rc:1}
     	     ]
     	
         },
@@ -153,22 +153,29 @@ onItemDropSection1(e: any){
 	let index1=0; 
 	let index11=0; 
 	let index2=0; 
-	let index22=0; 
+	let index22=0;
+	let fd1=false;
+	let fd2=false;
+	
 	this.section1.data.forEach((j,v)=>{
-		let fd=true;
 		j.forEach((j1,v1)=>{
 			if (e.nativeEvent.target.id === j1.idn+''+j1.idv){
 				index1=v1;
 				index11=v;
+				fd1=true;
 			}
 			if (e.dragData.idn+''+e.dragData.idv === j1.idn+''+j1.idv){
 				index2=v1;
 				index22=v;
+				fd2=true;
 			}
 		});
-		console.log(index1+"---"+index2);
-		if (index11==index22){
+		console.log(index1+"---"+index2);//2 //1
+		console.log("-------------");//2 //1
+		console.log(index11+"---"+index22+"--"+fd1+"---"+fd2);//2 //1
+		if (index11==index22 && fd1 && fd2){
 			[j[index1], j[index2]] = [j[index2], j[index1]];
+			fd1=fd2=false;
 		}
 		temp1.push(j);
 		console.log(j);
@@ -182,7 +189,7 @@ onItemDropSection1(e: any){
 	 });
 	this.section1.data=temp1;
 	
-	if (index11!=index22){
+	if (index11!=index22 && fd1 && fd2){
 		
 		
 		[this.section1.data[index11][index1], this.section1.data[index22][index2]] = 
@@ -224,29 +231,16 @@ createSectionData(d:any){
 	document.getElementById("section-1").innerHTML=thtml;	
    }
 
-}
-/*
 
-{
-	 section:1,
-	 name: "main section",
-	 data:
-   { 
-    a:[
-		{name: "Office<br/> Name", type: "dpf",value:"Lavaca", dt:false,idn:1,idv:21},
-		{name: "Patient<br/> Name", type: "fill",value:"Test", dt:false,idn:2,idv:22},
-		{name: "Insurance<br/> Name", type: "fill",value:"abc", dt:false,idn:3,idv:23},
-		{name: "Tax ID", type: "fill",value:"113", dt:false,idn:4,idv:24},
-		{name: "Patient<br/> DOB", type: "fill",value:"01/01/2006", dt:true,idn:5,idv:25}
-	     ],
-     b:[
-{name: "Office<br/> Name", type: "dpf",value:"Lavaca", dt:false,idn:1,idv:21},
-		{name: "Patient<br/> Name", type: "fill",value:"Test", dt:false,idn:2,idv:22},
-		{name: "Insurance<br/> Name", type: "fill",value:"abc", dt:false,idn:3,idv:23},
-		{name: "Tax ID", type: "fill",value:"113", dt:false,idn:4,idv:24},
-		{name: "Patient<br/> DOB", type: "fill",value:"01/01/2006", dt:true,idn:5,idv:25}
-       ]
-     }
-     
+saveFinalData(){
+	    
+	 this.items=[];
+	 this.items.push(this.section1);
+	 this.items.push(this.section2);
+	 //this.items.push();
+	 //this.items.push();
+	 console.log(this.items);
+	 
+ }
+
 }
-*/
