b2de157abc37a00936542eb7e5178a1fc680132d
deepakcardinal <deepak.dogra@cardinalts.com>
2022-12-28 23:00:16 +0530

    Transaction issues fixed

--- rule_engine/rcm/src/main/java/com/tricon/rcm/scheduler/ClaimScheduler.java
+++ rule_engine/rcm/src/main/java/com/tricon/rcm/scheduler/ClaimScheduler.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tricon.rcm.db.entity.RcmOffice;
 import com.tricon.rcm.db.entity.RcmUser;
@@ -36,6 +37,7 @@ public class ClaimScheduler {
 	RcmCommonServiceImpl commonsService;
 
 	@Scheduled(cron = "${scheduler.startcron}")
+	@Transactional(rollbackFor = Exception.class)
 	public void updateClaimAndInsuranceWithUnBilledClaimsFromRE() {
 
 		logger.info("ClaimScheduler Run at :-" + new Date());
