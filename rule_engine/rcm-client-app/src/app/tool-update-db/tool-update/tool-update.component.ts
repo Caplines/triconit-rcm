dbf6b9b648b9cf7e1f66e5fe210cd60ad195f7b3
deepakcardinal <deepak.dogra@cardinalts.com>
2023-02-15 17:45:52 +0530

    tool to update RCm updated

--- rule_engine/rcm-client-app/src/app/tool-update-db/tool-update/tool-update.component.ts
+++ rule_engine/rcm-client-app/src/app/tool-update-db/tool-update/tool-update.component.ts
@@ -39,12 +39,12 @@ export class ToolUpdateComponent implements OnInit {
 /* Sample Response
 {"message":"","data":[{"source":"EAGLESOFT","status":"1","newClaimsCount":28,"officeUuid":"0078ceb5-5c6b-11e9-8320-0627f79ab72a","cd":"2022-12-28T17:29:25.000+00:00"},{"source":"EAGLESOFT","status":"1","newClaimsCount":27,"officeUuid":"fa484adb-ff40-11eb-882d-06f98f9a5400","cd":"2023-01-25T12:57:33.000+00:00"},{"source":"EAGLESOFT","status":"1","newClaimsCount":9,"officeUuid":"fc1d7afd-7df2-11e8-8432-8c16451459cd","cd":"2023-01-25T13:19:42.000+00:00"},{"source":"","status":"0","newClaimsCount":0,"officeUuid":"f015515d-7df2-11e8-8432-8c16451459cd","cd":null},{"source":"","status":"0","newClaimsCount":0,"officeUuid":"e5eec389-aaaf-11e8-8544-8c16451459cd","cd":null},{"source":"","status":"0","newClaimsCount":0,"officeUuid":"c04a2dbe-9bc5-11e8-9f0b-8c16451459cd","cd":null},{"source":"","status":"0","newClaimsCount":0,"officeUuid":"a0f6c4ec-5bd6-11e9-8320-0627f79ab72a","cd":null},{"source":"","status":"0","newClaimsCount":0,"officeUuid":"065b3c52-57b5-11e9-8320-0627f79ab72a","cd":null},{"source":"","status":"0","newClaimsCount":0,"officeUuid":"be2c3847-aaae-11e8-8544-8c16451459cd","cd":null},{"source":"","status":"0","newClaimsCount":0,"officeUuid":"da0c77a8-aaaf-11e8-8544-8c16451459cd","cd":null},{"source":"","status":"0","newClaimsCount":0,"officeUuid":"e9f3d445-aaaf-11e8-8544-8c16451459cd","cd":null},{"source":"","status":"0","newClaimsCount":0,"officeUuid":"cc450da8-aaae-11e8-8544-8c16451459cd","cd":null},{"source":"","status":"0","newClaimsCount":0,"officeUuid":"e25ac8c4-aaaf-11e8-8544-8c16451459cd","cd":null},{"source":"","status":"0","newClaimsCount":0,"officeUuid":"c0f64f25-9bc5-11e8-9f0b-8c16451459cd","cd":null},{"source":"","status":"0","newClaimsCount":0,"officeUuid":"dec0c6f2-aaaf-11e8-8544-8c16451459cd","cd":null},{"source":"","status":"0","newClaimsCount":0,"officeUuid":"c0f73cbd-9bc5-11e8-9f0b-8c16451459cd","cd":null},{"source":"","status":"0","newClaimsCount":0,"officeUuid":"edc6f1c5-aaaf-11e8-8544-8c16451459cd","cd":null}],"status":200}
 */
-console.log(this.cName);
+this.sourceType="";
+
      ths.appService.fetchLatesClaimLLogs(this.cName,(res:any)=>{
        
        if (res.status=== 200){
         ths.log=res.data;
-        console.log(ths.log);
 
 
        }else{
@@ -58,6 +58,11 @@ console.log(this.cName);
   pullFreshClaims(){
     let ths=this;
     ths.freshClaimPullModel.officeuuids=[];
+   
+    if (this.sourceType =='' &&  this.smilePoint.uuid==this.cName) {
+      console.log("Please select Source");
+      return;
+    } 
     ths.setSource();
     ths.log.forEach(e => {
       if (e.update) ths.freshClaimPullModel.officeuuids.push(e.officeUuid);
@@ -69,7 +74,6 @@ console.log(this.cName);
       this.appService.pullFreshClaims(ths.freshClaimPullModel,(res:any)=>{
         if (res.status=== 200){
           ths.loader=false;
-           debugger;
           res.data.forEach((d:FreshClaimPLogs) => {
              
              let filteredData:Array<FreshClaimPLogs> = ths.log.filter((l) => l.officeUuid === d.officeUuid);
@@ -103,7 +107,9 @@ console.log(this.cName);
 
  setSource(){
   let ths= this;
-  if (ths.sourceType=='') ths.freshClaimPullModel.source='GOOGLESHEET';
+  console.log(ths.sourceType);
+   if (ths.sourceType=='') ths.freshClaimPullModel.source='GOOGLESHEET';
+   else  ths.freshClaimPullModel.source='EAGLESOFT';
  /* if (ths.esAgent || ths.googleSheet){
     if (ths.esAgent) ths.freshClaimPullModel.source="EAGLESOFT";
     else ths.freshClaimPullModel.source="GOOGLESHEET";
