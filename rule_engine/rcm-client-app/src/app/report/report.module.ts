7e2aceb807340af42b847e8bf0c984b7d86198f3
ayush.srivastava <ayush.srivastava@triconitsolutions.com>
2023-02-24 20:13:03 +0530

    unused  code removed

--- rule_engine/rcm-client-app/src/app/report/report.module.ts
+++ rule_engine/rcm-client-app/src/app/report/report.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthService } from "../service/auth-service.service";
-import { TokenInterceptor } from '../auth/token.interceptor';
-//import { HttpModule , XSRFStrategy, CookieXSRFStrategy, Http} from "@angular/http";
 import { FormsModule } from '@angular/forms';
 import { HeaderModule } from '../header/header.module';
 import { ReportComponent } from './report.component';
 import { ReportRoutingModule } from './report.routing.module';
-import { NgDatepickerComponent, NgDatepickerModule } from 'ng2-datepicker';
-import { ApplicationService } from '../service/application.service';
 
 
 @NgModule({
@@ -20,10 +15,9 @@ import { ApplicationService } from '../service/application.service';
     CommonModule,
     ReportRoutingModule, FormsModule,
     HeaderModule,
-    NgDatepickerModule
 
   ],
-  providers: [AuthService,ApplicationService],
+  providers: [AuthService],
   exports:[ReportComponent]
     //OfficeResolve,
    /* {
