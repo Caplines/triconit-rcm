a8bea0a32d654e25504b6c36884eaa02dee13836
deepakcardinal <deepak.dogra@cardinalts.com>
2023-03-08 02:51:32 +0530

    ui fix

--- rule_engine/rcm-client-app/src/environments/environment.prod.ts
+++ rule_engine/rcm-client-app/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URL : "http://18.221.170.13"
+  API_URL : "http://18.221.170.13",
+  recaptcha: {siteKey: '6LdMb8ckAAAAANA4rC581AqxQma5C2gYRvFE8yVE'}
 };
