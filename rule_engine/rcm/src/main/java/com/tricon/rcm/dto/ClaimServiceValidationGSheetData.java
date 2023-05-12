7b5888b6aa3ca613a72299457460d123b56439e6
deepakcardinal <deepak.dogra@cardinalts.com>
2023-05-12 14:52:50 +0530

    ClaimServiceValidationGSheetData missed file added

--- rule_engine/rcm/src/main/java/com/tricon/rcm/dto/ClaimServiceValidationGSheetData.java
+++ rule_engine/rcm/src/main/java/com/tricon/rcm/dto/ClaimServiceValidationGSheetData.java
@@ -5,8 +5,13 @@ import lombok.Data;
 @Data
 public class ClaimServiceValidationGSheetData {
 
-	String heading;
-	String description;
-	String value;
+	//String heading;
+	String description;//5
+	String value;//6
+	String team;//1
+	String nameOfService;//2
+	String values;//4
+	String autoOrManual;//3
+	
 	
 }
