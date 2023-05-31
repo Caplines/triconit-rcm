e68054a697c702bd4a4e86a09e0fb850770c0589
deepakcardinal <deepak.dogra@cardinalts.com>
2023-05-31 21:00:58 +0530

    Enable tool to update for Internal Audit

--- rule_engine/rcm/src/main/java/com/tricon/rcm/api/controller/RcmController.java
+++ rule_engine/rcm/src/main/java/com/tricon/rcm/api/controller/RcmController.java
@@ -90,7 +90,7 @@ public class RcmController extends BaseHeaderController{
 			return ResponseEntity.ok(new GenericResponse(HttpStatus.BAD_REQUEST, "", "not Autorized"));
 		}
 		
-		if (!(partialHeader.getTeamId()==RcmTeamEnum.BILLING.getId() || partialHeader.getTeamId()!=RcmTeamEnum.INTERNAL_AUDIT.getId())) {
+		if (!(partialHeader.getTeamId()==RcmTeamEnum.BILLING.getId() || partialHeader.getTeamId()==RcmTeamEnum.INTERNAL_AUDIT.getId())) {
 			return ResponseEntity.ok(new GenericResponse(HttpStatus.BAD_REQUEST, "", "not Autorized"));
 		}
 		Object sucess = null;
