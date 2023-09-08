deca236e6896d0c52e5e5e55ac44add74d675461
deepakcardinal <deepak.dogra@cardinalts.com>
2023-09-08 13:39:50 +0530

    Allow and Not Allow to Upload Files

--- rule_engine/rcm-client-app/src/app/shared/attach-file/attach-file.component.ts
+++ rule_engine/rcm-client-app/src/app/shared/attach-file/attach-file.component.ts
@@ -10,43 +10,43 @@ import { DownLoadService } from 'src/app/service/download.service';
 })
 export class AttachFileComponent {
 
-  showModal:boolean=false;
-  selectedFiles:any=[];
-  totalFile:number=0;
-  loader:boolean=false;
-  formData:any = new FormData();
-  @Input() inputConfig:any={};
-  @Output() emitToParent:any= new EventEmitter();
-  attachmentTypeId:any;
-  attachedFiles:any=[];
-  hasAttachmentFileData:boolean=false;
-  errorMessage:any;
+  showModal: boolean = false;
+  selectedFiles: any = [];
+  totalFile: number = 0;
+  loader: boolean = false;
+  formData: any = new FormData();
+  @Input() inputConfig: any = {};
+  @Output() emitToParent: any = new EventEmitter();
+  attachmentTypeId: any;
+  attachedFiles: any = [];
+  hasAttachmentFileData: boolean = false;
+  errorMessage: any;
 
-  removeAttachmentFiles:any=[];
-  removeClaimAttachmentId:any=[];
+  removeAttachmentFiles: any = [];
+  removeClaimAttachmentId: any = [];
 
-  constructor(public constant:AppConstants,private appService:ApplicationServiceService,private downloadService:DownLoadService){}
+  constructor(public constant: AppConstants, private appService: ApplicationServiceService, private downloadService: DownLoadService) { }
 
   openModal() {
     this.showModal = true;
-    if(this.inputConfig.attachmentCount>0 && !this.hasAttachmentFileData && this.removeAttachmentFiles.length==0){
+    if (this.inputConfig.attachmentCount > 0 && !this.hasAttachmentFileData && this.removeAttachmentFiles.length == 0) {
       this.getAttachmentFile();
     }
   }
 
-  getAttachmentFile(){
-    this.appService.getAttachmentFile(this.inputConfig.claimUuid,(res:any)=>{
-      if(res.status==200){
-          console.log(res);
-          this.attachedFiles = res.data;
-          // this.hasAttachmentFileData =  true;
-          // if(this.selectedFiles.length==0){
-          //   this.selectedFiles= res.data;
-          // } else{
-          //   res.data.forEach((ele:any) => {
-          //         this.selectedFiles.push(ele);
-          //   });
-          // }
+  getAttachmentFile() {
+    this.appService.getAttachmentFile(this.inputConfig.claimUuid, (res: any) => {
+      if (res.status == 200) {
+        console.log(res);
+        this.attachedFiles = res.data;
+        // this.hasAttachmentFileData =  true;
+        // if(this.selectedFiles.length==0){
+        //   this.selectedFiles= res.data;
+        // } else{
+        //   res.data.forEach((ele:any) => {
+        //         this.selectedFiles.push(ele);
+        //   });
+        // }
       }
     })
   }
@@ -59,75 +59,83 @@ export class AttachFileComponent {
   onFileChange(event: any) {
     this.formData = new FormData();
     for (let i = 0; i < event.target.files.length; i++) {
-      let params:any = {
-        'claimUuid':this.inputConfig.claimUuid,
-        'attachmentTypeId':'',
-        'file':event.target.files[i]
+      let params: any = {
+        'claimUuid': this.inputConfig.claimUuid,
+        'attachmentTypeId': '',
+        'file': event.target.files[i]
       }
       this.selectedFiles.unshift(params);
     }
   }
 
   removeItem(file: File) {
-    const index = this.selectedFiles.findIndex((e:any)=>e.file == file);
+    const index = this.selectedFiles.findIndex((e: any) => e.file == file);
     if (index !== -1) {
       this.selectedFiles.splice(index, 1);
       this.totalFile = this.selectedFiles.length;
     }
   }
 
-  uploadItem(claimUuid:any) {
-    let fileNameExist:Boolean =  this.isFileNameExist();
-    let isEmptyAttachment:Boolean = this.isEmptyAttachmentType();
-    if(!fileNameExist && !isEmptyAttachment){
+  uploadItem(claimUuid: any) {
+    let fileNameExist: Boolean = this.isFileNameExist();
+    let isEmptyAttachment: Boolean = this.isEmptyAttachmentType();
+    if (!fileNameExist && !isEmptyAttachment) {
       this.totalFile = this.selectedFiles.length;
-      this.emitToParent.emit({action:'fileSelected',value:this.selectedFiles,claimUuid:claimUuid,});
+      this.emitToParent.emit({ action: 'fileSelected', value: this.selectedFiles, claimUuid: claimUuid, });
 
-      if(this.removeAttachmentFiles){
-        this.emitToParent.emit({action:'filesSelectedToRemove',value:this.removeAttachmentFiles,claimUuid:claimUuid});
+      if (this.removeAttachmentFiles) {
+        this.emitToParent.emit({ action: 'filesSelectedToRemove', value: this.removeAttachmentFiles, claimUuid: claimUuid });
       }
-      this.errorMessage='';
+      this.errorMessage = '';
       this.closeModal();
-    } else{
-      fileNameExist ?  this.errorMessage = "Same File Already Exist" : this.errorMessage = "Please Select Attachment Type";
+    } else {
+      fileNameExist ? this.errorMessage = "Same File Already Exist" : this.errorMessage = "Please Select Attachment Type";
     }
   }
 
-  isFileNameExist(){
-    return this.selectedFiles.some((obj:any, index:any) =>
-    this.selectedFiles.slice(index + 1).some((innerObj:any) => innerObj.file.name === obj.file.name));
- }
-
-  isEmptyAttachmentType(){
-     return this.selectedFiles.some((item:any)=>item.attachmentTypeId == '');
- }
-
- removePreSelectedFile(file:any){
-  let deleteFile = confirm("Are You Sure You Want To Delete ?");
-  if(deleteFile){
-  const index = this.attachedFiles.findIndex((e:any)=>e.file.name == file.file.name);
-  if (index !== -1) {
-    this.attachedFiles.splice(index, 1);
-    this.inputConfig.attachmentCount = this.attachedFiles.length;
-    this.removeClaimAttachmentId.push(file.id);
-    let params:any= {
-      "claimAttachmentId":this.removeClaimAttachmentId,
-      "claimUuid":this.inputConfig.claimUuid
-    };
-    this.removeAttachmentFiles = params;
+  isFileNameExist() {
+    return this.selectedFiles.some((obj: any, index: any) =>
+      this.selectedFiles.slice(index + 1).some((innerObj: any) => innerObj.file.name === obj.file.name));
   }
- }
-}
 
-downloadAttachment(file:any){
-      this.appService.downloadAttachments({'fileId':file.id},(res:any)=>{
-        if(res.status ==200){
-            console.log(res);
-            this.downloadService.saveBolbData(res.body,file.file.name)
-        }
-      })
-}
+  isEmptyAttachmentType() {
+    return this.selectedFiles.some((item: any) => item.attachmentTypeId == '');
+  }
 
-  
+  removePreSelectedFile(file: any) {
+    let deleteFile = confirm("Are You Sure You Want To Delete ?");
+    if (deleteFile) {
+      const index = this.attachedFiles.findIndex((e: any) => e.file.name == file.file.name);
+      if (index !== -1) {
+        this.attachedFiles.splice(index, 1);
+        this.inputConfig.attachmentCount = this.attachedFiles.length;
+        this.removeClaimAttachmentId.push(file.id);
+        let params: any = {
+          "claimAttachmentId": this.removeClaimAttachmentId,
+          "claimUuid": this.inputConfig.claimUuid
+        };
+        this.removeAttachmentFiles = params;
+      }
+    }
+  }
+
+  downloadAttachment(file: any) {
+    this.appService.downloadAttachments({ 'fileId': file.id }, (res: any) => {
+      if (res.status == 200) {
+        console.log(res);
+        this.downloadService.saveBolbData(res.body, file.file.name)
+      }
+    })
+  }
+
+  get allowUpload(): boolean {
+    //False = u can upload
+    console.log(this.inputConfig.allowUpload);
+    if (this.inputConfig.allowUpload != undefined && this.inputConfig.allowUpload) {
+      return this.inputConfig.allowUpload;
+    } else {
+      return false;
+    }
+  }
 
 }
