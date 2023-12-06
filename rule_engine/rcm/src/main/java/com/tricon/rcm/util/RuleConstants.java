ba83b44e545c30eedab6fe25bc4daf8b2e6afb57
deepakcardinal <deepak.dogra@cardinalts.com>
2023-12-06 19:55:49 +0530

    Claim to Claim

--- rule_engine/rcm/src/main/java/com/tricon/rcm/util/RuleConstants.java
+++ rule_engine/rcm/src/main/java/com/tricon/rcm/util/RuleConstants.java
@@ -9,7 +9,7 @@ public class RuleConstants {
 	public static final String RULE_ID_301="ES vs. IV Comparison - Member ID";
 	public static final String RULE_ID_302="ES vs. IV Comparison - Group Number";
 	public static final String RULE_ID_303="ES vs. IV Comparison - Patient/Subscriber Details";
-	public static final String RULE_ID_304="Treating Provider vs. Provider on Claim";//
+	public static final String RULE_ID_304="Treating Provider vs. Provider on claim";//
 	public static final String RULE_ID_305="Credentialing Status";
 	//public static final String RULE_ID_306="Assignment of Benefits";//This was removed in Phase 2
 	public static final String RULE_ID_307="CRA Code";//CRA Code Availability
