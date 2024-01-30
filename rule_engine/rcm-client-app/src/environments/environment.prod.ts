40fc6e1c31a0fee04afed26d452d71a23c3ddb9d
deepakcardinal <deepak.dogra@cardinalts.com>
2024-01-30 17:11:47 +0530

    SSL issue Fix

--- rule_engine/rcm-client-app/src/environments/environment.prod.ts
+++ rule_engine/rcm-client-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL : "http://18.221.170.13",
-  recaptcha: {siteKey: '6LdMb8ckAAAAANA4rC581AqxQma5C2gYRvFE8yVE'}
+  API_URL: "https://caplinedentalbilling.com",
+  recaptcha: { siteKey: '6LdMb8ckAAAAANA4rC581AqxQma5C2gYRvFE8yVE' }
 };
