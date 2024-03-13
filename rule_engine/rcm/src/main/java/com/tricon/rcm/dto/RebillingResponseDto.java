d4be5325dec104669c39b17c708658a6aa2826ad
Navneet Shukla <navneet.shukla@triconitsolutions.com>
2024-03-13 22:35:27 +0530

    REBILLING RESPONSE DTO

---
+++ rule_engine/rcm/src/main/java/com/tricon/rcm/dto/RebillingResponseDto.java
@@ -0,0 +1,18 @@
+package com.tricon.rcm.dto;
+
+import lombok.Data;
+
+@Data
+public class RebillingResponseDto {
+
+	private String requestedRemarks;
+	private String rebillingRemarks;
+	private String reasonForRebilling;
+	private String rebillingRequirements;
+	private String rebillingServiceCodes;
+	private String requestedBy;
+	private String requestedByUuid;
+	private boolean rebillingStatus;
+	private String dateOfRebiiling;
+	private int sectionId;
+}
