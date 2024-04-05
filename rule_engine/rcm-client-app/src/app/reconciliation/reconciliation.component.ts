215de1c61c3d44d4587621cabf87ffc5bb1bd8b1
Ayush Srivastava <ayushsrivastava1011@gmail.com>
2024-04-05 18:33:03 +0530

    Reconcile hardcoded data added

--- rule_engine/rcm-client-app/src/app/reconciliation/reconciliation.component.ts
+++ rule_engine/rcm-client-app/src/app/reconciliation/reconciliation.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import { ApplicationServiceService } from '../service/application-service.service';
 
 @Component({
   selector: 'app-reconciliation',
   standalone: true,
-  imports: [CommonModule,FormsModule,RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './reconciliation.component.html',
   styleUrls: ['./reconciliation.component.scss']
 })
@@ -15,53 +16,66 @@ export class ReconciliationComponent {
 
   private title = inject(Title);
 
-  reconcilData:any=[];
-  loader:boolean=false;
+  reconcilData: any = [];
+  loader: boolean = false;
+  officeData: any = [];
 
-  constructor(){
+  constructor(private _service: ApplicationServiceService) {
     this.title.setTitle("RCM TOOL - Reconciliation");
   }
 
-  ngOnInit(){
+  ngOnInit() {
+    this.fetchOffice();
 
-    this.loader =true;
-    setTimeout(()=>{
+  }
+
+  fetchOffice() {
+    this._service.fetchOfficeData((res: any) => {
+      if (res) {
+        console.log(res);
+        this.officeData = res.data;
+      }
+    })
+  }
+
+  getReconcileData() {
+    this.loader = true;
+    setTimeout(() => {
       this.reconcilData = [
         {
-          title:"Primary & Secondary Unbilled",
-          claimsES:100,
-          claimsRCM:200,
-          discrepancies:0 
+          title: "Primary & Secondary Unbilled",
+          claimsES: 100,
+          claimsRCM: 200,
+          discrepancies: ['htttps://google.com','htttps://google.com']
         },
         {
-          title:"Primary Open & Secondary Unbilled",
-          claimsES:100,
-          claimsRCM:200,
-          discrepancies:0 
+          title: "Primary Open & Secondary Unbilled",
+          claimsES: 100,
+          claimsRCM: 200,
+          discrepancies: ['htttps://google.com']
         },
         {
-          title:"Primary Closed & Secondary Unbilled",
-          claimsES:100,
-          claimsRCM:200,
-          discrepancies:0 
+          title: "Primary Closed & Secondary Unbilled",
+          claimsES: 100,
+          claimsRCM: 200,
+          discrepancies: ['htttps://google.com']
         },
         {
-          title:"Primary Closed & Secondary Open",
-          claimsES:100,
-          claimsRCM:200,
-          discrepancies:0 
+          title: "Primary Closed & Secondary Open",
+          claimsES: 100,
+          claimsRCM: 200,
+          discrepancies: ['htttps://google.com']
         },
         {
-          title:"Primary & Secondary Closed",
-          claimsES:100,
-          claimsRCM:200,
-          discrepancies:0 
+          title: "Primary & Secondary Closed",
+          claimsES: 100,
+          claimsRCM: 200,
+          discrepancies: ['htttps://google.com']
         },
       ];
-      this.loader=false;
-  },3000
+      this.loader = false;
+    }, 0
     )
-        
-}
+  }
 
 }
