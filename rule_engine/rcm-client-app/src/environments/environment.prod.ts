4ace9228baf9334d3d16f0579706655bdb12f269
heyhexadecimal <hex@protonmail.com>
2026-02-25 15:21:04 +0530

    updated the captcha key

--- rule_engine/rcm-client-app/src/environments/environment.prod.ts
+++ rule_engine/rcm-client-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API_URL: "/api/v1",
-  recaptcha: { siteKey: '6LdMb8ckAAAAANA4rC581AqxQma5C2gYRvFE8yVE' }
+  recaptcha: { siteKey: '6LcDOXcsAAAAAKbja4SPv9iTQojTh38SeglzZt3T' }
 };
